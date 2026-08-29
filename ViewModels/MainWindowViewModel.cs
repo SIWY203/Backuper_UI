f9803dd37b7237fc3e05a6819f7abee18c18bf79
SIWY203 <cefalaspis12345@gmail.com>
2026-08-30 01:31:03 +0200

    working on converting console to wpf

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,14 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿using System.Windows.Input;
 namespace Backuper_UI.ViewModels;
 
 public class MainWindowViewModel
 {
+    public ICommand AddClusterCommand { get; }
+    public ICommand RemoveClusterCommand { get; }
+    public ICommand OpenSettingsCommand { get; }
+
     public MainWindowViewModel()
     {
-
-
+        AddClusterCommand = new RelayCommand(ClusterUI.RunCreator);
+        RemoveClusterCommand = new RelayCommand(ClusterUI.RunRemover);
+        OpenSettingsCommand = new RelayCommand(SettingsUI.Settings);
     }
 }
