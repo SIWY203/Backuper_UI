dc127c14430ccdc9db089d01b0961dd60866ee07
SIWY203 <cefalaspis12345@gmail.com>
2026-08-31 23:38:41 +0200

    added new settings view

---
+++ ViewModels/SettingsWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+namespace Backuper_UI.ViewModels;
+
+public class SettingsWindowViewModel
+{
+    // Kolekcja automatycznie odświeżająca widok po dodaniu/usunięciu elementów
+    public ObservableCollection<string> Languages { get; } = ["English", "Polish"];
+
+    public string? SelectedLanguage { get; set; }
+
+    public int BackupLimit { get; set; } = Cleaner.CurrentLimit.MaxBackupCount;
+    public int SnapshotLimit { get; set; } = Cleaner.CurrentLimit.MaxSnapshotCount;
+
+    public SettingsWindowViewModel()
+    {
+        
+    }
+}
---
+++ Views/SettingsWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Backuper_UI.Views;
+
+/// <summary>
+/// Interaction logic for SettingsWindow.xaml
+/// </summary>
+public partial class SettingsWindow : Window
+{
+    public SettingsWindow()
+    {
+        InitializeComponent();
+    }
+
+}
